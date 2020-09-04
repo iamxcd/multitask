1163fbd294933e71a464252c8e404bf24ff17498
online <songbai_x@qq.com>
2020-09-04 10:12:27 +0800

    调整 子进程可以接收参数

--- src/Woker.php
+++ src/Woker.php
@@ -8,15 +8,19 @@ use Exception;
 class Woker
 {
     /**
-     * 进程数量
+     * 分片处理 参数
      */
-    private  $count;
+    private  $params;
 
+    /**
+     * 待执行的函数
+     */
     private $func;
 
-    public function setCount(int $num)
+
+    public function addParams($params)
     {
-        $this->count = $num;
+        $this->params[] = $params;
         return $this;
     }
 
@@ -28,16 +32,24 @@ class Woker
 
     public function run()
     {
-        $pid = pcntl_fork();
-
-        if ($pid == -1) {
-            throw new Exception('子进程创建失败');
-        } else if ($pid) {
-            // 等待子进程中断，防止子进程成为僵尸进程。
-            pcntl_wait($status);
-        } else {
-            $func = $this->func;
-            $func();
+        $count = count($this->params);
+        if (!$count) {
+            throw new Exception('参数不能为空');
         }
+
+        if (!($this->func instanceof Closure)) {
+            throw new Exception('任务不能为空');
+        }
+        for ($i = 0; $i < $count; $i++) {
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                throw new Exception('子进程创建失败');
+            } else if ($pid) {
+            } else {
+                call_user_func($this->func, $this->params[$i], $this);
+                die;
+            }
+        }
+        pcntl_wait($status);
     }
 }
--- test.php
+++ test.php
@@ -4,10 +4,18 @@ require_once './vendor/autoload.php';
 
 use Iamxcd\Multitask\Woker;
 
-
-
 $woker = new Woker();
 
-$woker->setCount(10)->task(function () {
-    echo '123';
+$url = "https://learnku.com/laravel?page=";
+for ($i = 1; $i <= 10; $i++) {
+    $woker->addParams($url . $i);
+}
+
+$path = './html/';
+if (!is_dir($path)) {
+    mkdir($path);
+}
+$woker->task(function ($params, $workr) use ($path) {
+    $file_name = md5(mt_rand(1000, 9999) . time()) . '.txt';
+    file_put_contents($path . $file_name, file_get_contents($params));
 })->run();
